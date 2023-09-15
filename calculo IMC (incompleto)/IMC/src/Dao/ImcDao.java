5b65206d0b73b80bac20e8caf2a6cb04f1c6beed
Pablo Massao Lins Bunno <regigigaspower2@gmail.com>
2023-09-15 08:20:10 -0300

    Update ImcDao.java

--- calculo IMC (incompleto)/IMC/src/Dao/ImcDao.java
+++ calculo IMC (incompleto)/IMC/src/Dao/ImcDao.java
@@ -2,7 +2,43 @@ package Dao;
 
 import Model.PessoaModel;
 import Controller.ImcController;
+import java.util.List;package Dao;
+
+import Model.PessoaModel;
+import Controller.ImcController;
 import java.util.List;
+import java.util.ArrayList;
+
+public class ImcDao {
+
+	private List<PessoaModel> listaDePessoas;
+
+	public ImcDao() {
+		listaDePessoas = new ArrayList<>();
+
+	}
+
+	public void adicionarPessoa(PessoaModel pessoaModel) {
+		double imc = calcularImc(pessoaModel);
+		pessoaModel.setImc(imc);
+		listaDePessoas.add(pessoaModel);
+
+	}
+
+	private double calcularImc(PessoaModel pessoaModel) {
+		// TODO Auto-generated method stub
+		return pessoaModel.getImc();
+	}
+
+	public List<PessoaModel> obterLista() {
+		return listaDePessoas;
+
+	}
+
+
+}
+
+
 import java.util.ArrayList;
 
 public class ImcDao {
