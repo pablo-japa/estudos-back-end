d664464155f254d9cff99d2d5913d6e3dc1699df
Pablo Massao Lins Bunno <regigigaspower2@gmail.com>
2023-09-29 09:29:24 -0300

    Update ListaView.java

--- revisão/revisao/src/view/ListaView.java
+++ revisão/revisao/src/view/ListaView.java
@@ -1,5 +1,11 @@
 package view;
 
 public class ListaView {
+	
+	public void limparlista() {
+		System.out.println("------------------");
+		System.out.println("A lista foi limpar");
+		System.out.println("------------------");
+	}
 
 }
